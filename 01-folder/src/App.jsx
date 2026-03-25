94e33c16647706f2d23e1cfa7726db824e0824d5
DevSachin25 <sachinkr9971315766@gmail.com>
2026-03-25 17:50:05 +0530

    First Folder in React

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello from React</h1>
+      <h2>I am checking the code.</h2>
+    </div>
+  )
+}
+
+export default App

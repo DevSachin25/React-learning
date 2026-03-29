87598b68dee4e5f5bf7c5a5f1260ced00ef752ae
DevSachin25 <sachinkr9971315766@gmail.com>
2026-03-29 13:13:15 +0530

    Fragments-Learning

---
+++ learning-fragments/src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Healthy Food </h1>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">Dal</li>
+        <li class="list-group-item">Roti</li>
+        <li class="list-group-item">Ghee</li>
+        <li class="list-group-item">Green Vegetables</li>
+        <li class="list-group-item">Milk</li>
+      </ul>
+    </>
+  );
+}
+
+export default App;
